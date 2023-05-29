47253dea2374f9d47b5dab0cb3325745ef809074
Sergey Moiseev <skifride@gmail.comm>
2023-05-29 20:09:08 +0300

    add task7

---
+++ Task7/Program.cs
@@ -0,0 +1,31 @@
+﻿int x1 = Promt("Введите координату x: ");
+int y1 = Promt("Введите координату y: ");
+FindCoordinate(x1,y1);
+
+
+int Promt(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+void FindCoordinate(int x, int y)
+{
+    if(x>0 && y>0)
+    {
+        Console.WriteLine("I четверть");
+    }
+    if(x<0 && y>0)
+    {
+        Console.WriteLine("II четверть");
+    }
+    if(x<0 && y<0)
+    {
+        Console.WriteLine("III четверть");
+    }
+    if(x>0 && y<0)
+    {
+        Console.WriteLine("IV четверть");
+    }
+}
