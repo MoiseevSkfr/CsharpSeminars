92dd808407311867e89409bbb5b82d7f3d6ac824
Sergey Moiseev <skifride@gmail.comm>
2023-06-01 16:08:34 +0300

    add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+//4 -> 24
+//5 -> 120
+
+Console.Clear();
+string mess = "Введите число:";
+int num = Prompt(mess);
+int result = GetProductNums(num);
+Console.WriteLine($"Произведение чисел от 1 до N = {result}");
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int GetProductNums(int number)
+{
+    int res = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        res = res * i;
+        Console.WriteLine(res + " ");
+    }
+    return res;
+}
